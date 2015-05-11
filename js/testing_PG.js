381cb5e01b3e3d74be1ac269091036f13af0adf4
Evans55 <jonathanccoicca@gmail.com>
2015-05-11 18:18:28 -0500

    ddsdds

--- js/testing_PG.js
+++ js/testing_PG.js
@@ -121,7 +121,7 @@ function onDeviceReady(){
     var id =active_page.page().attr('id');
     if (id==='page') {
         if (confirm('¿Seguro que quieres cerrar la aplicación?')==true){
-            app.exitApp();
+            navigator.app.exitApp();
         }
     }
     else{
